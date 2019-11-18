cf41b10f1aa3195f1f508524d3d85cb3e8ec9cf6
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2019-11-18 01:09:09 -0500

    python files for heroku

--- smallboard/settings.py
+++ smallboard/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Configure Django App for Heroku.
 import django_heroku
