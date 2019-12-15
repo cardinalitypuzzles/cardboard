fe44deb0e2fef66f4b706bc7bed13f57213ec84a
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2019-12-15 15:32:58 -0500

    reserve tag colors

--- puzzles/puzzle_tag.py
+++ puzzles/puzzle_tag.py
@@ -4,20 +4,23 @@ from django.db import models
 class PuzzleTag(TagBase):
     BLUE = "primary"
     GRAY = "secondary"
-    GREEN = "success"
-    RED = "danger"
-    YELLOW = "warning"
+    GREEN = "success"       # reserved for back solved
+    RED = "danger"          # reserved for high pri
+    YELLOW = "warning"      # reserved for low pri
     WHITE = "light"
-    BLACK = "dark"
-    COLORS = [
-        (BLUE, "blue"),
-        (GRAY, "gray"),
-        (GREEN, "green"),
-        (RED, "red"),
-        (YELLOW, "yellow"),
-        (WHITE, "white"),
-        (BLACK, "black")
-    ]
+    BLACK = "dark"          # reserved for meta tags
+    COLORS = {
+        BLUE : "blue",
+        GRAY : "gray",
+        GREEN : "green",
+        RED : "red",
+        YELLOW : "yellow",
+        WHITE : "white",
+        BLACK : "black"
+    }
+    RESERVED = [GREEN, RED, YELLOW, BLACK]
+    VISIBLE_COLOR_CHOICES = filter(lambda c : c[0] not in PuzzleTag.RESERVED, COLORS.items())
+
     COLOR_ORDERING = {
         RED: 0,
         BLACK: 1,
@@ -29,7 +32,7 @@ class PuzzleTag(TagBase):
     }
     color = models.CharField(
         max_length=10,
-        choices=COLORS,
+        choices=COLORS.items(),
         default=BLUE)
     # internal flag to know when to sync meta puzzles
     is_meta = models.BooleanField(default=False)
