20edc10d6f87e09c7f1e4e753b0e63ba7bc7ba20
asdfryan <asdfryan123@gmail.com>
2019-12-14 17:15:26 -0800

    Add hierarchical sorting based on metas

--- puzzles/templatetags/puzzle_extras.py
+++ puzzles/templatetags/puzzle_extras.py
@@ -12,9 +12,51 @@ DEFAULT_TAGS = [
 
 register = template.Library()
 
+def generate_sortkeys(puzzles):
+    sortkey_dict = {}
+    # TODO(asdfryan): This will fail if there are cycles. We should add cycle detection.
+    def __get_sortkey(puzzle):
+        def __get_status_sortkey(puzzle):
+            ''' Unsolved puzzles should come before solved puzzles.'''
+            if puzzle.is_solved():
+                return "1-%s" % puzzle.pk
+            return "0-%s" % puzzle.pk
+
+        def __add_to_dict_and_return(pk, sortkey):
+            sortkey_dict[pk] = sortkey
+            print("sortkey for puzzle %s: %s" % (Puzzle.objects.get(id=pk).name, sortkey))
+            return sortkey
+
+        if puzzle.pk in sortkey_dict: return sortkey_dict[puzzle.pk]
+
+        # If non-meta and has no assigned meta, it comes first.
+        if not puzzle.is_meta and not puzzle.has_assigned_meta():
+            return __add_to_dict_and_return(puzzle.pk, __get_status_sortkey(puzzle))
+
+        # If it has an assigned meta puzzle, concatenate parent sortkey with current status sortkey.
+        if puzzle.has_assigned_meta():
+            print("metas length: %i" % len(puzzle.metas.all()))
+            return __add_to_dict_and_return(puzzle.pk,
+                "%s.%s" % (__get_sortkey(puzzle.metas.all()[0]), __get_status_sortkey(puzzle)))
+
+        return __add_to_dict_and_return(puzzle.pk, "0.%s" % __get_status_sortkey(puzzle))
+
+    for p in puzzles:
+        __get_sortkey(p)
+
+    return sortkey_dict
+
+
 @register.inclusion_tag('puzzles_table.html')
 def get_table(puzzles, request):
-    status_forms = [StatusForm(initial={'status': p.status}) for p in puzzles]
+    sortkeys = generate_sortkeys(puzzles)
+    sorted_puzzles = sorted(puzzles, key=lambda p: sortkeys[p.pk])
+
+    def __get_depth(puzzle):
+        return max(0, sortkeys[puzzle.pk.count('-')] - 1)
+
+    depth = [__get_depth(puzzle) for p in sorted_puzzles]
+    status_forms = [StatusForm(initial={'status': p.status}) for p in sorted_puzzles]
     for (i, p) in enumerate(puzzles):
         if p.status in [Puzzle.SOLVED, Puzzle.PENDING]:
             status_forms[i].fields["status"].disabled = True
@@ -22,16 +64,16 @@ def get_table(puzzles, request):
             status_forms[i].fields["status"].choices =\
                 [(status, status) for status in Puzzle.VISIBLE_STATUS_CHOICES]
 
-    meta_forms = [MetaPuzzleForm(initial={'metas': p.metas.all()}, instance=p) for p in puzzles]
+    meta_forms = [MetaPuzzleForm(initial={'metas': p.metas.all()}, instance=p) for p in sorted_puzzles]
 
-    edit_forms = [PuzzleForm(initial={'name': p.name, 'url': p.url, 'is_meta': p.is_meta}) for p in puzzles]
+    edit_forms = [PuzzleForm(initial={'name': p.name, 'url': p.url, 'is_meta': p.is_meta}) for p in sorted_puzzles]
 
     # this caches Puzzle.tags.all() for all the tag forms
     Puzzle.objects.all().prefetch_related('tags')
-    tag_forms = [TagForm() for p in puzzles]
+    tag_forms = [TagForm() for p in sorted_puzzles]
 
     context = {
-        'rows': zip(puzzles, status_forms, meta_forms, edit_forms, tag_forms),
+        'rows': zip(sorted_puzzles, status_forms, meta_forms, edit_forms, tag_forms, depth),
         'guess_form': AnswerForm(),
         'slack_base_url': settings.SLACK_BASE_URL,
     }
