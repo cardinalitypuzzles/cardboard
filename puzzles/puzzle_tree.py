82bc84ab8bb4087dc29cd52efa307be9b53f6d43
asdfryan <asdfryan123@gmail.com>
2019-12-15 00:09:19 -0800

    Add puzzle tree class and some unit tests

---
+++ puzzles/puzzle_tree.py
@@ -0,0 +1,93 @@
+from .models import *
+
+class PuzzleNode:
+    ''' Wrapper class around a puzzle with parent and child pointers. '''
+    puzzle = None
+    parents = []
+    children = []
+
+    def __init__(self, puzzle):
+        self.puzzle = puzzle
+
+    def __str__(self):
+        def __root_str(node):
+            return node.puzzle.__str__() if node.puzzle else "ROOT"
+        root_puzzle = __root_str(self)
+        parents_str = ",".join([__root_str(node) for node in self.parents])
+        children_str = ",".join([__root_str(node) for node in self.children])
+        return "root: %s\nparents: %s\nchildren: %s" % (root_puzzle, parents_str, children_str)
+
+    def add_child(self, child):
+        if child == self:
+            print("Why is it adding to itself?")
+        self.children.append(child)
+
+    def getSortedPuzzles(self):
+        output = [self]
+        def __sortkey(node):
+            '''
+            Unsolved children should come before SOLVED children.
+            After that, childless should come first.
+            '''
+            return (node.puzzle.status == Puzzle.SOLVED, len(node.children) > 0)
+
+        #print ("Calling getSortedPuzzles() on puzzle %s" % self)
+        for child in sorted(self.children, key=__sortkey):
+            #print ("Child %s" % child)
+
+            output.append(child.getSortedPuzzles())
+
+        return output
+
+
+class PuzzleTree:
+    '''
+    Tree representation of a set of puzzles. Each node is represented by a puzzle.
+    A node is a parent node of another node if the former is a metapuzzle for the latter.
+    Note that this is not *really* a tree since we allow nodes to have multiple parents in
+    order to support multiple metas per puzzle.
+    '''
+
+    def __init__(self, puzzles):
+        ''' Initialize tree. It is assumed that puzzles contains unique puzzles (no dups). '''
+        # A fake node to contain all the metaless puzzles as children.
+        self.root = PuzzleNode(None)
+        # Construct nodes for each puzzle
+        # Dictionary mapping puzzle PK -> list of nodes
+        self.puzzle_to_node = {p.pk : PuzzleNode(p) for p in puzzles}
+
+        # Construct parent/child edges
+        for p in puzzles:
+            node = self.puzzle_to_node[p.pk]
+            if not p.has_assigned_meta():
+                print("root puzzle before: %s" % node)
+                print(id(self.root))
+                print(id(node))
+                #self.root.add_child(node)
+                for p in puzzles:
+                    node = self.puzzle_to_node[p.pk]
+                    print("ryan node %s" % node)
+                #self.root.children.append(node) # This also adds node to node.children for some reason
+                node.parents = [self.root]
+                print ("root puzzle after: %s" % node)
+            else:
+                node.parents = [self.puzzle_to_node[meta.pk] for meta in p.metas.all()]
+                print ("nonroot puzzle before: %s" % node)
+                for parent_node in node.parents:
+                    parent_node.children.append(node)
+                    print("parent puzzle after: %s" % parent_node)
+                print("nonroot puzzle after: %s" % node)
+                print("parent puzzle after: %s" % parent_node)
+        
+        print("Edges constructed!!!!!!")
+        for p in puzzles:
+            node = self.puzzle_to_node[p.pk]
+            print("node %s" % node)
+
+    def getSortedPuzzles(self):
+        '''
+        Returns a list of PuzzleNodes representing the ordering in which the puzzles should be
+        displayed in the table. Note that because of multiple parent support, some nodes may be
+        duplicated in the list.
+        '''
+        return self.root.getSortedPuzzles()[1:]
--- puzzles/tests.py
+++ puzzles/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
 
-# Create your tests here.
+from hunts.models import *
+from .models import *
+from .puzzle_tree import *
+
+class TestPuzzleTree(TestCase):
+    def setUp(self):
+        self._test_hunt = Hunt.objects.create(name="fake hunt", url="google.com")
+        self._suffix = 0
+        self._puzzles = []
+
+    def tearDown(self):
+        for p in self._puzzles:
+            p.delete()
+        self._test_hunt.delete()
+
+    def create_puzzle(self, name, is_meta=False):
+        fake_url = "fakeurl%i.com" % self._suffix
+        fake_sheet = "fakesheet%i.com" % self._suffix
+        fake_channel = str(self._suffix)
+        self._suffix += 1
+
+        puzzle = Puzzle.objects.create(name=name, hunt=self._test_hunt, url=fake_url,
+                                       sheet=fake_sheet, channel=fake_channel)
+        self._puzzles.append(puzzle)
+        return puzzle
+
+
+    #def test_empty(self):
+    #    self.assertEqual(PuzzleTree([]).getSortedPuzzles(), [])
+
+    def test_basic(self):
+        meta1 = self.create_puzzle("meta1", True)
+        puzzle1_1 = self.create_puzzle("puzzle1-1")
+        puzzle1_2 = self.create_puzzle("puzzle1-2")
+
+        puzzle1_1.metas.add(meta1)
+        puzzle1_2.metas.add(meta1)
+
+        puzzle1_1.status = Puzzle.SOLVED
+
+        puzzle_dangling = self.create_puzzle("puzzle_dangling")
+
+        sorted_nodes = PuzzleTree([puzzle1_1, puzzle1_2, meta1, puzzle_dangling]).getSortedPuzzles()
+        expected = ["puzzle_dangling", "meta1", "puzzle1-2", "puzzle1-1"]
+        self.assertEqual([node.puzzle.__str__() for node in sorted_nodes], expected)
