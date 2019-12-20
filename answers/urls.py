17d640e814bf74de7e888b93f477f83d6ccff372
Anthony Hsu <erwaman@gmail.com>
2019-12-19 20:42:23 -0500

    Fix Answer Queue page (#89)

--- answers/urls.py
+++ answers/urls.py
@@ -4,8 +4,8 @@ from django.http import HttpResponseRedirect
 from django.urls import path
 
 urlpatterns = [
-    path("queue/<int:hunk_pk>", AnswerView.as_view()),
-    path("queue/<int:hunk_pk>/<int:answer_pk>", AnswerView.as_view()),
+    path("queue/<int:hunt_pk>", AnswerView.as_view()),
+    path("queue/<int:hunt_pk>/<int:answer_pk>", AnswerView.as_view()),
     path("update_note/<int:answer_pk>", update_note, name="update_note"),
     path('', lambda r: HttpResponseRedirect('queue/{}'.format(settings.ACTIVE_HUNT_ID)), name="queue"),
 ]
