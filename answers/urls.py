c987e5f40c3205546d10d3685dc9873c794f8447
Akira Baruah <akira.baruah@gmail.com>
2020-11-28 20:21:00 -0800

    Rename hunt_queue pattern to submission_queue

--- answers/urls.py
+++ answers/urls.py
@@ -7,13 +7,13 @@ from hunts.views import LastAccessedHuntRedirectView
 
 app_name = "answers"
 urlpatterns = [
-    path("queue/<slug:hunt_slug>", AnswerView.as_view(), name="hunt_queue"),
+    path("queue/<slug:hunt_slug>", AnswerView.as_view(), name="submission_queue"),
     path("queue/<slug:hunt_slug>/answers", answers),
     path("queue/<slug:hunt_slug>/<int:answer_pk>", AnswerView.as_view()),
     path("update_note/<int:answer_pk>", update_note, name="update_note"),
     path(
         "",
-        LastAccessedHuntRedirectView.as_view(pattern_name="answers:hunt_queue"),
+        LastAccessedHuntRedirectView.as_view(pattern_name="answers:submission_queue"),
         name="queue",
     ),
 ]
