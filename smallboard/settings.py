14263fae2a7fa515c6b5fbb797a3b2ad7f107c4d
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2020-11-30 02:36:11 -0800

    Hook up initial React view
    Visible at /hunts/react/{id}

--- smallboard/settings.py
+++ smallboard/settings.py
@@ -137,7 +137,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "smallboard/static")]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "smallboard/static"),
+    os.path.join(BASE_DIR, "hunts/static"),
+]
 STATICFILES_STORAGE = "whitenoise.django.CompressedManifestStaticFilesStorage"
 
 # User login
