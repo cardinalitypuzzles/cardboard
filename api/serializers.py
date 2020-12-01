f9f6b45ab9c5898194b122c34e2c0f972c18b177
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2020-11-30 18:19:55 -0800

    Add initial puzzles API
    And hook it up with react-tables. Obviously a lot more stuff to do but
    the basic parts are there.

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,32 @@
 from rest_framework import serializers
 from hunts.models import Hunt
+from puzzles.models import Puzzle
 
 
 class HuntSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hunt
         fields = ("id", "name", "active")
+
+
+class PuzzleSerializer(serializers.ModelSerializer):
+    tags = serializers.SerializerMethodField()
+
+    def get_tags(self, obj):
+        return [{"name": tag.name, "color": tag.color} for tag in obj.tags.all()]
+
+    class Meta:
+        model = Puzzle
+        fields = (
+            "id",
+            "name",
+            "hunt_id",
+            "url",
+            "notes",
+            "sheet",
+            "status",
+            "answer",
+            "tags",
+            "metas",
+            "is_meta",
+        )
--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("v1/hunt/<int:pk>/", views.HuntAPIView.as_view(), name="hunt_api_view"),
+    path("v1/hunt/<int:pk>", views.HuntAPIView.as_view(), name="hunt_api_view"),
+    path(
+        "v1/hunt/<int:pk>/puzzles", views.AllPuzzles.as_view(), name="all_puzzles_api"
+    ),
 ]
