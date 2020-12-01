7486f1778caef4128789e9fa12741fd89d5ce888
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2020-11-30 20:56:03 -0800

    Sort the table

--- hunts/src/hunt-view.js
+++ hunts/src/hunt-view.js
@@ -101,20 +101,45 @@ export class HuntViewMain extends React.Component {
         ];
     }
 
+    _puzzleComparator(a, b) {
+        // Solved puzzles should appear below unsolved ones
+        if (a.status == "SOLVED" && b.status != "SOLVED") {
+            return 1;
+        } else if (b.status == "SOLVED" && a.status != "SOLVED") {
+            return -1;
+        }
+        // Feeders before metas
+        if (a.feeders.length == 0 && b.feeders.length > 0) {
+            return -1;
+        } else if (a.feeders.length > 0 && b.feeders.length == 0) {
+            return 1;
+        }
+        // Newer puzzles before old ones
+        // TODO: once creation times are added to puzzles, use those instead
+        return b.id - a.id;
+    }
+
     _processPuzzleData(puzzleData) {
         const rowMap = {};
         puzzleData.forEach(row => {
            rowMap[row.id] = row;
         });
         puzzleData.forEach(row => {
-            if (row.feeders && row.feeders.length > 0) {
+            if (row.feeders.length > 0) {
                 row.subRows = [];
                 row.feeders.forEach(subRowId => {
                     row.subRows.push(rowMap[subRowId]);
                 });
             }
         });
-        return Object.values(rowMap).filter(row => row.metas.length == 0);
+        puzzleData.forEach(row => {
+            if (row.subRows) {
+                row.subRows.sort(this._puzzleComparator);
+            }
+        });
+        const rows = Object.values(rowMap).filter(row => row.metas.length == 0);
+        rows.sort(this._puzzleComparator);
+        return rows;
     }
 
     render() {
