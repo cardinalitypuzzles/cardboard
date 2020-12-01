7b843294137c79326a8f06342a9912c21481c4a3
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2020-11-30 18:19:55 -0800

    Add initial puzzles API
    And hook it up with react-tables. Obviously a lot more stuff to do but
    the basic parts are there.

--- hunts/src/hunt-view.js
+++ hunts/src/hunt-view.js
@@ -1,14 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { PuzzleTable} from "./puzzle-table.js";
 
 export class HuntViewMain extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            puzzleData: [],
+        };
+        this.columns = this._tableColumns();
     }
 
     componentDidMount() {
         const huntApiUrl = `/api/v1/hunt/${this.props.huntId}`;
+        const puzzlesApiUrl = `/api/v1/hunt/${this.props.huntId}/puzzles`;
         fetch(huntApiUrl)
             .then(response => {
                 if (response.status > 400) {
@@ -24,13 +29,63 @@ export class HuntViewMain extends React.Component {
                     };
                 });
             });
+
+        fetch(puzzlesApiUrl)
+            .then(response => {
+                if (response.status > 400) {
+                    // TODO: error handling
+                    console.error("Puzzles API failure", response);
+                }
+                return response.json();
+            })
+            .then(puzzleData => {
+                this.setState(() => {
+                    return {
+                        puzzleData
+                    };
+                });
+            });
+    }
+
+    _tableColumns() {
+        return [
+            {
+                Header: "Name",
+                accessor: "name",
+            },
+            {
+                Header: "Answer",
+                accessor: "answer",
+            },
+            {
+                Header: "Status",
+                accessor: "status",
+            },
+            {
+                Header: "Sheet",
+                accessor: "sheet",
+            },
+            {
+                Header: "Tags",
+                id: "tags",
+            },
+            {
+                Header: "Metas",
+                accessor: "metas",
+            },
+        ];
     }
 
     render() {
         if (this.state.huntData) {
-            return <h1>{this.state.huntData.name}</h1>;
+            return (
+            <div>
+                <h1>{this.state.huntData.name} - All Puzzles</h1>
+                <PuzzleTable columns={this.columns} data={this.state.puzzleData} />
+            </div>
+            );
         } else {
-            return <h1>Hello world!</h1>;
+            return <h1>Loading...</h1>;
         }
     }
 }
