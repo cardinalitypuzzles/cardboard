6d1139e8a68b154c7cd088098905f4dc5a4e5271
Peter Vera <petercvera@gmail.com>
2020-12-05 21:32:13 -0800

    add prettier to our codebase and run it over the existing javascript (#244)
    * add prettier to our codebase and run it over the existing javascript
    
    * add prettier to precommit
    
    * Fix pre-commit config for prettier
    
    Adjusts the indentation to make the pre-commit yaml parser happy. Also
    re-runs `pre-commit run --all-files` to reformat some files.
    
    Co-authored-by: Peter Vera <pcvera@google.com>
    Co-authored-by: Akira Baruah <akira.baruah@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    devtool: 'source-map',
-    watch: true,
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    }
+  devtool: "source-map",
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
 };
