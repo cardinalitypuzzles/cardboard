86f3497b20cf65cc788d099f86408cd0972ed82c
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2020-12-07 13:09:00 -0800

    Switch puzzles API to a ViewSet
    fancy magic stuff

--- api/views.py
+++ api/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import viewsets
 
 from hunts.models import Hunt
+from puzzles.models import Puzzle
 from .serializers import HuntSerializer, PuzzleSerializer
 
 # Right now IsAuthenticated ensures that only logged-in users
@@ -20,11 +22,14 @@ class HuntAPIView(APIView):
         return Response(serializer.data)
 
 
-class AllPuzzles(APIView):
+class PuzzleViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
+    serializer_class = PuzzleSerializer
 
-    def get(self, request, pk):
-        hunt = get_object_or_404(Hunt, pk=pk)
-        puzzles = hunt.puzzles.all().prefetch_related("metas").prefetch_related("tags")
-        serializer = PuzzleSerializer(puzzles, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        hunt_id = self.kwargs["hunt_id"]
+        return (
+            Puzzle.objects.filter(hunt__id=hunt_id)
+            .prefetch_related("metas")
+            .prefetch_related("tags")
+        )
