afe10f5bda03779f76bdb478c2a8f4d56e6fb130
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2020-12-07 13:09:00 -0800

    Switch puzzles API to a ViewSet
    fancy magic stuff

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
+puzzle_list = views.PuzzleViewSet.as_view(
+    {
+        "get": "list",
+    }
+)
+
 urlpatterns = [
     path("v1/hunt/<int:pk>", views.HuntAPIView.as_view(), name="hunt_api_view"),
-    path(
-        "v1/hunt/<int:pk>/puzzles", views.AllPuzzles.as_view(), name="all_puzzles_api"
-    ),
+    path("v1/hunt/<int:hunt_id>/puzzles", puzzle_list, name="puzzle_list"),
 ]
