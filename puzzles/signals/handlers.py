ce177bc7afeee9e0d1570e166064a1ed5f9f8b64
Sushi <rawxfish@users.noreply.github.com>
2021-01-12 00:32:25 -0500

    Rename sheet when puzzle is deleted (#373) (#383)
    * Rename sheet when puzzle is deleted (#373)
    
    * black
    
    * move to signals
    
    * rename

--- puzzles/signals/handlers.py
+++ puzzles/signals/handlers.py
@@ -76,7 +76,7 @@ def update_tags_m2m(sender, instance, action, reverse, model, pk_set, **kwargs):
 
 
 @receiver(pre_delete, sender=Puzzle)
-def update_meta_sheets_pre_delete(sender, instance, **kwargs):
+def update_sheets_pre_delete(sender, instance, **kwargs):
     # Need to be careful with the closure here:
     # instance.metas.all() will be empty after the transaction commits,
     # so we need to copy the metas out in advance
@@ -88,6 +88,13 @@ def update_meta_sheets_pre_delete(sender, instance, **kwargs):
 
     if google_api_lib.enabled():
         transaction.on_commit(update_metas)
+
+        if instance.sheet:
+            transaction.on_commit(
+                lambda: google_api_lib.task.rename_sheet.delay(
+                    sheet_url=instance.sheet, name=f"[DELETED] {instance.name}"
+                )
+            )
 
 
 @receiver(m2m_changed, sender=Puzzle.metas.through)
