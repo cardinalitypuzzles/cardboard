b6bdd4c766cc4b244b0f8a8fa25ebe8e4a2cd915
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2021-01-14 04:43:42 -0500

    ahhhhhh

--- smallboard/routing.py
+++ smallboard/routing.py
@@ -1,6 +1,5 @@
 from django.urls import re_path
 
-from smallboard.asgi import django_asgi_app
 from smallboard.consumers import NotificationConsumer
 
 from channels.auth import AuthMiddlewareStack
@@ -11,7 +10,6 @@ from channels.security.websocket import AllowedHostsOriginValidator
 
 application = ProtocolTypeRouter(
     {
-        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(
                 URLRouter(
