dd31f43c46182708310a0a5680411af7cc6d1114
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2021-01-14 11:06:47 -0500

    Revert "add logging"
    This reverts commit 0ceffba77a68685d518d7d271919364b3a2db92a.

--- smallboard/consumers.py
+++ smallboard/consumers.py
@@ -5,7 +5,6 @@ from asgiref.sync import async_to_sync
 
 class NotificationConsumer(WebsocketConsumer):
     def connect(self):
-        print(self.scope)
         if self.scope["user"].is_anonymous:
             self.close()
         else:
