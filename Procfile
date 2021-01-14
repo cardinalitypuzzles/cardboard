6b27131ad6897bc6fd9c7dbe2c96209ee9537a42
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2021-01-14 05:15:20 -0500

    test without websocket

--- Procfile
+++ Procfile
@@ -1,5 +1,5 @@
 release: python manage.py migrate --noinput
-web: daphne smallboard.routing:application --port $PORT --bind 0.0.0.0 -v2
+web: daphne smallboard.asgi:django_asgi_app --port $PORT --bind 0.0.0.0 -v2
 worker: celery -A smallboard worker -l INFO --without-heartbeat --without-gossip --without-mingle
 bot: python manage.py rundiscordbot
 channel: python manage.py runworker channel_layer -v2
--- smallboard/routing.py
+++ smallboard/routing.py
@@ -12,17 +12,6 @@ from channels.security.websocket import AllowedHostsOriginValidator
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(
-                URLRouter(
-                    [
-                        re_path(
-                            r"^notifications/(?P<hunt_pk>[0-9]+)$",
-                            NotificationConsumer.as_asgi(),
-                        ),
-                    ]
-                )
-            )
-        ),
+
     }
 )
