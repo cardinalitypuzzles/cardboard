980458529c6f6d60001fcfe31b092140fcdd98c2
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2021-01-14 04:20:02 -0500

    or maybe this

--- smallboard/routing.py
+++ smallboard/routing.py
@@ -1,13 +1,12 @@
 from django.urls import re_path
 
+from smallboard.asgi import django_asgi_app
 from smallboard.consumers import NotificationConsumer
-
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
-from smallboard.asgi import django_asgi_app
 
 
 application = ProtocolTypeRouter(
