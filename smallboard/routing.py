3bacb38fe3a8d2367b53b09eb9d7e16749355eb9
Shuxin (Sushi) Zhan <shuxinzhan@gmail.com>
2021-01-15 00:16:37 -0500

    bork AllowedHostsOriginValidator

--- smallboard/routing.py
+++ smallboard/routing.py
@@ -5,22 +5,19 @@ from smallboard.consumers import NotificationConsumer
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 
 
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            AuthMiddlewareStack(
-                URLRouter(
-                    [
-                        re_path(
-                            r"^notifications/(?P<hunt_pk>[0-9]+)$",
-                            NotificationConsumer.as_asgi(),
-                        ),
-                    ]
-                )
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                [
+                    re_path(
+                        r"^notifications/(?P<hunt_pk>[0-9]+)$",
+                        NotificationConsumer.as_asgi(),
+                    ),
+                ]
             )
         ),
     }
