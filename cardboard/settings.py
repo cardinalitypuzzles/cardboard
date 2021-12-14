b9e212d848277be3ec038817f580b0db4556cd90
Anthony Hsu <erwaman@gmail.com>
2021-12-14 18:49:38 -0500

    Run "black" on settings.py and sync_tasks.py

--- cardboard/settings.py
+++ cardboard/settings.py
@@ -249,18 +249,16 @@ except KeyError as e:
     )
 
 SOCIAL_AUTH_PIPELINE = (
-    'social_core.pipeline.social_auth.social_details',
-    'social_core.pipeline.social_auth.social_uid',
-
+    "social_core.pipeline.social_auth.social_details",
+    "social_core.pipeline.social_auth.social_uid",
     # custom auth_allowed check in lieu of default one
-    'google_api_lib.sync_tasks.auth_allowed',
-
-    'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username',
-    'social_core.pipeline.user.create_user',
-    'social_core.pipeline.social_auth.associate_user',
-    'social_core.pipeline.social_auth.load_extra_data',
-    'social_core.pipeline.user.user_details',
+    "google_api_lib.sync_tasks.auth_allowed",
+    "social_core.pipeline.social_auth.social_user",
+    "social_core.pipeline.user.get_username",
+    "social_core.pipeline.user.create_user",
+    "social_core.pipeline.social_auth.associate_user",
+    "social_core.pipeline.social_auth.load_extra_data",
+    "social_core.pipeline.user.user_details",
 )
 
 # Taggit Overrides
