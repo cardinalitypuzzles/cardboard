a934fe9f04a863e4b841fda32e1e078a9cf5e0fc
Peter Vera <petercvera@gmail.com>
2022-12-29 18:01:17 -0800

    use better chevrons for the expansion toggle

--- hunts/src/NameCell.js
+++ hunts/src/NameCell.js
@@ -5,6 +5,7 @@ import { faEdit, faTrashAlt } from "@fortawesome/free-regular-svg-icons";
 import { showModal } from "./modalSlice";
 import ClickableIcon from "./ClickableIcon";
 import { toggleCollapsed } from "./collapsedPuzzlesSlice";
+import { IconChevronDown, IconChevronRight } from "@tabler/icons";
 
 const useToggleRowExpandedProps = (row) => {
   const dispatch = useDispatch();
@@ -31,7 +32,8 @@ export default function NameCell({ row, value }) {
     <>
       {row.canExpand ? (
         <span {...toggleRowExpandedProps}>
-          {row.isExpanded ? "▼" : "▶"} <b>{value}</b>
+          {row.isExpanded ? <IconChevronDown /> : <IconChevronRight />}
+          <b>{value}</b>
         </span>
       ) : (
         <span style={{ paddingLeft: `${row.depth * 2}rem` }}>
