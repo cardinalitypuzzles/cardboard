b736bdebf5e2d416939bea9d3dc6b8c5c5de0072
Ryan Gossiaux <ryan.gossiaux@gmail.com>
2023-12-21 16:55:14 -0800

    Fix collectstatic and run it automatically in Docker
    `collectstatic` has been kind of broken in dev because the main.js file
    generated by webpack references some source map files in node_modules, and
    Django doesn't like the references to files that it doesn't know about.
    
    This webpack plugin extracts these source map references, which fixes the
    collectstatic issue and probably also fixes source maps for 3rd party
    libraries if they were broken.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,7 @@ yarn install
 yarn run dev &
 
 python manage.py migrate
+python manage.py collectstatic --no-input
 
 # Password is burrito (in docker-compose.yaml)
 python manage.py createsuperuser --noinput --username admin --email cardinalitypuzzles@gmail.com
