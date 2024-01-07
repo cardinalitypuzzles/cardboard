60718f55a03ec0c437f156dba2d2d0bd9dd084a2
maximized <watermaximillion@gmail.com>
2024-01-07 13:28:08 -0800

    Chrome Extension changes to support assigning the meta in the extension

--- chrome_extension/cardboard/popup.js
+++ chrome_extension/cardboard/popup.js
@@ -1,10 +1,37 @@
+const IS_PROD = true;
+
+const PROD_URL = "https://cardboard.rocks";
+const DEV_URL = "http://localhost:8000";
+
+// TODO: Make this configurable
+const HUNT_NUMBER = 10;
+
+let URL = DEV_URL;
+if (IS_PROD) {
+  URL = PROD_URL;
+}
+
+// Read all puzzles and filter down to those that are metas.
+const hunt_puzzles = await fetch(
+  URL + "/api/v1/hunts/" + HUNT_NUMBER + "/puzzles",
+  {
+    method: "GET",
+  }
+);
+const response = await hunt_puzzles.json();
+let metas = [];
+for (const puzzle of response) {
+  if (puzzle.is_meta) {
+    metas.push(puzzle.name);
+  }
+}
+
 // There should only be one tab because there is only one currentWindow
 // and active tab.
 const tabs = await chrome.tabs.query({
   active: true,
   currentWindow: true,
 });
-
 const template = document.getElementById("li_template");
 const elements = new Set();
 for (const tab of tabs) {
@@ -16,21 +43,32 @@ for (const tab of tabs) {
   if (tab.url != undefined) {
     element.querySelector(".puzzle_url").value = tab.url;
   }
+
+  // Add metas to dropdown
+  let meta_dropdown = element.querySelector(".puzzle_meta");
+  for (const meta of metas) {
+    let option = document.createElement("option");
+    option.text = meta;
+    option.value = meta;
+    option.key = meta;
+    meta_dropdown.add(option);
+  }
   elements.add(element);
 }
 document.querySelector("ul").append(...elements);
+
 const button = document.querySelector("button");
 button.addEventListener("click", async (e) => {
   e.preventDefault();
   // Get Cardboard cookie
   const cardboard_cookie = await chrome.cookies.get({
-    url: "https://cardboard.rocks",
+    url: URL,
     name: "csrftoken",
   });
 
   if (cardboard_cookie) {
     // Create puzzle. Puzzle name is limited to 30 characters
-    fetch("https://cardboard.rocks/api/v1/hunts/10/puzzles", {
+    fetch(URL + "/api/v1/hunts/" + HUNT_NUMBER + "/puzzles", {
       method: "POST",
       mode: "cors",
       body: JSON.stringify({
@@ -38,6 +76,7 @@ button.addEventListener("click", async (e) => {
         is_meta: document.getElementById("is_meta").checked,
         name: document.getElementById("puzzle_name").value.slice(0, 30),
         url: document.getElementById("puzzle_url").value,
+        assigned_meta: document.getElementById("puzzle_meta").value,
       }),
       headers: {
         "X-CSRFToken": cardboard_cookie.value,
