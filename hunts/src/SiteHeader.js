52c31e3ff39c3fea8e0fb298f07077db49f8f3e0
Nathan Pinsker <npinsker@gmail.com>
2024-01-09 17:17:25 -0800

    revert SiteHeader.js

--- hunts/src/SiteHeader.js
+++ hunts/src/SiteHeader.js
@@ -1,41 +1,17 @@
 import React from "react";
 import { DarkModeToggle } from "./DarkModeToggle";
-import Cookies from "js-cookie";
 import Drawer from "react-modern-drawer";
 import { ChatVersionSelector } from "./chatSelector";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { IconMenu2 } from "@tabler/icons";
 import "react-modern-drawer/dist/index.css";
-import { showAlert, hideAlert } from "./alertSlice";
 
 export const SiteHeader = () => {
-  const dispatch = useDispatch();
-
   const [isOpen, setIsOpen] = React.useState(false);
   const hunt = useSelector((state) => state.hunt);
   const toggleDrawer = () => {
     setIsOpen((prevState) => !prevState);
   };
-
-  let syncDiscordAndCardboardTags = (huntSlug) => {
-    const tagsApiUrl = `sync_discord_roles`;
-    fetch(tagsApiUrl, {
-      method: "POST",
-      headers: {
-        "X-CSRFToken": Cookies.get("csrftoken"),
-        "Content-Type": "application/json",
-      },
-    }).then(() => {
-      dispatch(
-        showAlert({
-          variant: "info",
-          text: "Discord roles updated successfully.",
-        })
-      );
-      setIsOpen(false);
-    });
-  };
-
   return (
     <>
       <nav
@@ -71,31 +47,34 @@ export const SiteHeader = () => {
           Logout
         </a>
         <div className="nav-link">Logged in as {window.LOGGED_IN_USER}</div>
-        <span
-          style={{
-            borderTop: "1px solid black",
-            width: "100%",
-            display: "inline-block",
-          }}
-          className="font-weight-bold nav-link"
-        >
-          Chat Settings
-        </span>
-        <div className="nav-link">
-          <ChatVersionSelector />
-        </div>
-        <span
-          style={{
-            borderTop: "1px solid black",
-            width: "100%",
-            display: "inline-block",
-          }}
-          className="font-weight-bold nav-link"
-        >
-          Appearance
-        </span>
-        <div className="nav-link">
-          <DarkModeToggle />
+
+        <div className="mt-auto" style={{ marginBottom: "36px" }}>
+          <span
+            style={{
+              borderTop: "1px solid black",
+              width: "100%",
+              display: "inline-block",
+            }}
+            className="font-weight-bold nav-link"
+          >
+            Chat Settings
+          </span>
+          <div className="nav-link">
+            <ChatVersionSelector />
+          </div>
+          <span
+            style={{
+              borderTop: "1px solid black",
+              width: "100%",
+              display: "inline-block",
+            }}
+            className="font-weight-bold nav-link"
+          >
+            Appearance
+          </span>
+          <div className="nav-link">
+            <DarkModeToggle />
+          </div>
         </div>
       </Drawer>
     </>
