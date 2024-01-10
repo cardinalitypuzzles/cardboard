5cd44f092a0ff92515bfb35080eb7344467b7baf
Nathan Pinsker <npinsker@gmail.com>
2024-01-09 17:18:19 -0800

    needless import

--- hunts/views.py
+++ hunts/views.py
@@ -6,7 +6,7 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
-from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
+from django.http import HttpResponseForbidden, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 from django.views import View
