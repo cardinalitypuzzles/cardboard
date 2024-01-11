bab967f3f165d286994318de3d6cd728a9fab140
Akira Baruah <akira.baruah@gmail.com>
2024-01-11 17:13:53 +0000

    Add button to create Discord channels in Hunt table

--- hunts/src/LinkCell.js
+++ hunts/src/LinkCell.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { CHAT_VERSION_OPTIONS, getChatVersion } from "./chatSlice";
 import { IconLink } from "./ClickableIcon";
 import { CHAT_PLATFORM, SHEET_REDIRECT_BASE } from "./constants";
+import { updatePuzzle } from "./puzzlesSlice";
 import { faPuzzlePiece } from "@fortawesome/free-solid-svg-icons";
 
 const createSheetLink = (puzzle) => {
@@ -25,7 +26,42 @@ const createDiscordLink = (puzzle, chatVersion) => {
   return puzzle.chat_room.text_channel_url;
 };
 
-const PurpleDiscordSvg = () => {
+const BlurpleDiscordSvg = () => <DiscordSvg fillColor="#5865F2" />;
+const LightBlurpleDiscordSvg = () => <DiscordSvg fillColor="#CCD0FB" />;
+const PulsingBlurpleDiscordSvg = () => {
+  return (
+    <>
+      <style>{`
+        .custom-discord-pulse > svg > * {
+          animation: pulse 1s infinite;
+          -webkit-animation: pulse 1s infinite;
+        }
+        @keyframes pulse {
+          0%   { fill: #5865F2 }
+          50%  { fill: #CCD0FB }
+          100% { fill: #5865F2 }
+        }
+        @-webkit-keyframes pulse {
+          0%   { fill: #5865F2 }
+          50%  { fill: #CCD0FB }
+          100% { fill: #5865F2 }
+        }
+      `}</style>
+      <span
+        className="custom-discord-pulse"
+        style={{
+          margin: "0 5px",
+          display: "inline-block",
+        }}
+        title="loading"
+      >
+        <DiscordSvg />
+      </span>
+    </>
+  );
+};
+
+const DiscordSvg = ({ fillColor }) => {
   return (
     <svg
       width="24px"
@@ -38,7 +74,7 @@ const PurpleDiscordSvg = () => {
       <g>
         <path
           d="M216.856339,16.5966031 C200.285002,8.84328665 182.566144,3.2084988 164.041564,0 C161.766523,4.11318106 159.108624,9.64549908 157.276099,14.0464379 C137.583995,11.0849896 118.072967,11.0849896 98.7430163,14.0464379 C96.9108417,9.64549908 94.1925838,4.11318106 91.8971895,0 C73.3526068,3.2084988 55.6133949,8.86399117 39.0420583,16.6376612 C5.61752293,67.146514 -3.4433191,116.400813 1.08711069,164.955721 C23.2560196,181.510915 44.7403634,191.567697 65.8621325,198.148576 C71.0772151,190.971126 75.7283628,183.341335 79.7352139,175.300261 C72.104019,172.400575 64.7949724,168.822202 57.8887866,164.667963 C59.7209612,163.310589 61.5131304,161.891452 63.2445898,160.431257 C105.36741,180.133187 151.134928,180.133187 192.754523,160.431257 C194.506336,161.891452 196.298154,163.310589 198.110326,164.667963 C191.183787,168.842556 183.854737,172.420929 176.223542,175.320965 C180.230393,183.341335 184.861538,190.991831 190.096624,198.16893 C211.238746,191.588051 232.743023,181.531619 254.911949,164.955721 C260.227747,108.668201 245.831087,59.8662432 216.856339,16.5966031 Z M85.4738752,135.09489 C72.8290281,135.09489 62.4592217,123.290155 62.4592217,108.914901 C62.4592217,94.5396472 72.607595,82.7145587 85.4738752,82.7145587 C98.3405064,82.7145587 108.709962,94.5189427 108.488529,108.914901 C108.508531,123.290155 98.3405064,135.09489 85.4738752,135.09489 Z M170.525237,135.09489 C157.88039,135.09489 147.510584,123.290155 147.510584,108.914901 C147.510584,94.5396472 157.658606,82.7145587 170.525237,82.7145587 C183.391518,82.7145587 193.761324,94.5189427 193.539891,108.914901 C193.539891,123.290155 183.391518,135.09489 170.525237,135.09489 Z"
-          fill="#5865F2"
+          fill={fillColor}
           fillRule="nonzero"
         />
       </g>
@@ -92,6 +128,19 @@ const GreenSheetsSvg = () => {
   );
 };
 
+const Clickable = ({ children, onClick, style, title }) => {
+  return (
+    <span
+      style={{ cursor: "pointer", ...style }}
+      className="text-muted"
+      onClick={onClick}
+      title={title}
+    >
+      {children}
+    </span>
+  );
+};
+
 const SvgLink = ({ children, url, style }) => {
   return (
     <a
@@ -116,6 +165,22 @@ export const LinkCell = ({ row }) => {
   const chatVersion = useSelector(getChatVersion);
   const hasDiscord = hasDiscordLink(puzzle);
   const discordLink = createDiscordLink(puzzle, chatVersion);
+
+  const dispatch = useDispatch();
+  const [loadingDiscord, setLoadingDiscord] = React.useState(false);
+
+  const createDiscordChannels = () => {
+    setLoadingDiscord(true);
+    dispatch(
+      updatePuzzle({
+        huntId: puzzle.hunt_id,
+        id: puzzle.id,
+        body: {
+          create_channels: true,
+        },
+      })
+    );
+  };
 
   return (
     <div
@@ -151,13 +216,25 @@ export const LinkCell = ({ row }) => {
           url={discordLink}
           style={{
             margin: "0 5px",
-            fill: "currentColor",
             display: "inline-block",
           }}
         >
-          <PurpleDiscordSvg />
+          <BlurpleDiscordSvg />
         </SvgLink>
-      ) : null}
+      ) : loadingDiscord ? (
+        <PulsingBlurpleDiscordSvg />
+      ) : (
+        <Clickable
+          onClick={createDiscordChannels}
+          style={{
+            margin: "0 5px",
+            display: "inline-block",
+          }}
+          title="Create channels"
+        >
+          <LightBlurpleDiscordSvg />
+        </Clickable>
+      )}
     </div>
   );
 };
