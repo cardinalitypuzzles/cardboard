efbd5ffc8624bb38f3286e80dee95c1c84316f90
maximized <watermaximillion@gmail.com>
2025-01-01 21:14:52 -0800

    Formatting

--- chrome_extension/cardboard/popup.js
+++ chrome_extension/cardboard/popup.js
@@ -11,30 +11,28 @@ const base_url = hunt_data["origin"];
 
 // Exit script if hunt information isn't found.
 if (hunt_id === undefined) {
-  throw new Error('No hunt_id found');
+  throw new Error("No hunt_id found");
 }
 if (hunt_url === undefined) {
-  throw new Error('No hunt_url found');
+  throw new Error("No hunt_url found");
 }
 if (base_url === undefined) {
-  throw new Error('No base_url found');
+  throw new Error("No base_url found");
 }
 
 // Get hunt name from site.
-const hunt_metadata = await fetch(
-  base_url + "/api/v1/hunts/" + hunt_id,
-  {
-    method: "GET",
-  }
-);
+const hunt_metadata = await fetch(base_url + "/api/v1/hunts/" + hunt_id, {
+  method: "GET",
+});
 const hunt_metadata_json = await hunt_metadata.json();
-document.getElementById("hunt_name_tooltip").title = "To change hunts, go to " + base_url + "/hunts/ and select a different hunt.";
+document.getElementById("hunt_name_tooltip").title =
+  "To change hunts, go to " + base_url + "/hunts/ and select a different hunt.";
 
 if (hunt_metadata_json.name != undefined) {
   document.getElementById("hunt_name").textContent = hunt_metadata_json.name;
-  document.getElementById("hunt_name").href = hunt_url;  
+  document.getElementById("hunt_name").href = hunt_url;
 } else {
-  document.getElementById("hunt_name").textContent = 'No hunt name found';
+  document.getElementById("hunt_name").textContent = "No hunt name found";
 }
 
 // Read all puzzles.
@@ -54,12 +52,12 @@ for (const puzzle of response) {
   if (puzzle.is_meta) {
     metas.push(puzzle.name);
   }
-  
+
   // Populate URL to Id map for all puzzles with sheets.
   if (puzzle.has_sheet) {
     puzzles_by_url.set(puzzle.url, puzzle.id);
   } else {
-    // Assign 
+    // Assign
     puzzles_by_url.set(puzzle.url, NO_SHEET_SENTINEL);
   }
 }
@@ -84,11 +82,14 @@ for (const tab of tabs) {
     if (puzzles_by_url.get(tab.url) != undefined) {
       // If the sheet has already been made, display a link to the sheet.
       if (puzzles_by_url.get(tab.url) != NO_SHEET_SENTINEL) {
-        document.getElementById("puzzle_message").textContent = "Puzzle already exists:";
+        document.getElementById("puzzle_message").textContent =
+          "Puzzle already exists:";
         document.getElementById("google_sheets_link").hidden = false;
-        document.getElementById("google_sheets_link").href = base_url + "/puzzles/s/" + puzzles_by_url.get(tab.url);
+        document.getElementById("google_sheets_link").href =
+          base_url + "/puzzles/s/" + puzzles_by_url.get(tab.url);
       } else {
-        document.getElementById("puzzle_message").textContent = "Puzzle already exists but the sheet is still being created.\nPlease wait 10 seconds then open the extension again.";
+        document.getElementById("puzzle_message").textContent =
+          "Puzzle already exists but the sheet is still being created.\nPlease wait 10 seconds then open the extension again.";
       }
     }
   }
@@ -109,7 +110,7 @@ document.querySelector("ul").append(...elements);
 function pollForCreatedPuzzlePeriodically(puzzle_url, interval, max_calls) {
   let call_count = 0;
   let interval_id = null;
-  
+
   async function pollAndCheck() {
     call_count++;
     const hunt_puzzles = await fetch(
@@ -122,11 +123,13 @@ function pollForCreatedPuzzlePeriodically(puzzle_url, interval, max_calls) {
     for (const puzzle of response) {
       // If the puzzle has been created and has a sheet, change the extension text to include a
       // link to the sheet and stop polling the website.
-      if (puzzle.url === puzzle_url & puzzle.has_sheet) {
+      if ((puzzle.url === puzzle_url) & puzzle.has_sheet) {
         clearInterval(interval_id);
         document.getElementById("google_sheets_link").hidden = false;
-        document.getElementById("puzzle_message").textContent = "Puzzle created:";
-        document.getElementById("google_sheets_link").href = base_url + "/puzzles/s/" + puzzle.id;
+        document.getElementById("puzzle_message").textContent =
+          "Puzzle created:";
+        document.getElementById("google_sheets_link").href =
+          base_url + "/puzzles/s/" + puzzle.id;
       }
     }
     if (call_count >= max_calls) {
@@ -134,7 +137,7 @@ function pollForCreatedPuzzlePeriodically(puzzle_url, interval, max_calls) {
       console.log("Max calls reached");
     }
   }
-  
+
   pollAndCheck();
   interval_id = setInterval(pollAndCheck, interval);
 }
@@ -165,10 +168,15 @@ button.addEventListener("click", async (e) => {
         "Content-Type": "application/json",
       },
     });
-    document.getElementById("puzzle_message").textContent = "Puzzle created, waiting for Google Sheet to be created...";
-    
+    document.getElementById("puzzle_message").textContent =
+      "Puzzle created, waiting for Google Sheet to be created...";
+
     // Poll website every 2 seconds for 30 seconds waiting for the sheet to be created.
-    pollForCreatedPuzzlePeriodically(document.getElementById("puzzle_url").value, 2000, 15);
+    pollForCreatedPuzzlePeriodically(
+      document.getElementById("puzzle_url").value,
+      2000,
+      15
+    );
   } else {
     console.log("No cookie found");
   }
--- hunts/src/App.js
+++ hunts/src/App.js
@@ -1,23 +1,26 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store";
 import { HuntViewMain } from "./HuntViewMain";
 
 const sendTokenToChromeExtension = ({ extensionId, huntId }) => {
-  chrome.runtime.sendMessage(extensionId, { huntId }, response => {
+  chrome.runtime.sendMessage(extensionId, { huntId }, (response) => {
     if (!response.success) {
-      console.log('Error sending message: ', response);
+      console.log("Error sending message: ", response);
       return response;
     }
   });
-}
+};
 
 const App = () => {
   useEffect(() => {
-    sendTokenToChromeExtension({ extensionId: 'fhldkjfidcbfienegpehemncionolmfa', huntId: window.CURRENT_HUNT_ID })
-  }, [])
-  
+    sendTokenToChromeExtension({
+      extensionId: "fhldkjfidcbfienegpehemncionolmfa",
+      huntId: window.CURRENT_HUNT_ID,
+    });
+  }, []);
+
   return <HuntViewMain huntId={window.CURRENT_HUNT_ID} />;
 };
 
