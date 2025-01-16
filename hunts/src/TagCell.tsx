02eead3c168d5f47a323f173a06466835b7fd7ed
Nathan Pinsker <npinsker@gmail.com>
2025-01-16 01:16:50 -0500

    menu/UI cleanup #3: click-to-edit on some columns (#851)
    * just click on cells to edit them
    
    * tweaks

--- hunts/src/TagCell.tsx
+++ hunts/src/TagCell.tsx
@@ -10,6 +10,7 @@ import type { RootState } from "./store";
 import type { Hunt, Puzzle, Row } from "./types";
 
 function TagCell({ row }: { row: Row<Puzzle> }) {
+  const [uiHovered, setUiHovered] = React.useState(false);
   const dispatch = useDispatch();
   const { id: huntId } = useSelector<RootState, Hunt>((state) => state.hunt);
   const puzzleId = row.original.id;
@@ -21,7 +22,31 @@ function TagCell({ row }: { row: Row<Puzzle> }) {
     : row.original.tags.filter((t) => !t.is_meta && t.name !== row.original.name);
 
   return (
-    <>
+    <div
+     onMouseEnter={() => {
+       setUiHovered(true);
+     }}
+     onMouseLeave={() => {
+       setUiHovered(false);
+     }}
+     onClick={() => {
+       dispatch(
+        showModal({
+          type: "EDIT_TAGS",
+          props: {
+            huntId,
+            puzzleId: row.values.id,
+            puzzleName: row.values.name,
+          },
+        })
+       );
+     }}
+     style={{
+       // TODO: abstract these properties out into their own CSS class
+       cursor: 'pointer',
+       minHeight: '1.4rem',
+       backgroundColor: uiHovered ? '#ffe579' : undefined
+     }}>
       {tagsToShow.map(({ name, color, id }) => (
         <TagPill
         name={name}
@@ -29,24 +54,8 @@ function TagCell({ row }: { row: Row<Puzzle> }) {
         key={name}
         onClick={() => dispatch(toggleFilterTag({ name, color, id }))}
         />
-      ))}{" "}
-
-      <ClickableIcon
-      icon={faTag}
-      onClick={() =>
-        dispatch(
-          showModal({
-            type: "EDIT_TAGS",
-            props: {
-              huntId,
-              puzzleId: row.values.id,
-              puzzleName: row.values.name,
-            },
-          })
-        )
-      }
-      />{" "}
-    </>
+      ))}
+    </div>
   );
 }
 
